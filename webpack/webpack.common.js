c745663b4057a715d70c74d932a4ea3c22ed3313
fzz14hao <fzz14hao@163.com>
2020-09-07 16:01:43 +0800

    增加 多页面配置

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -5,6 +5,7 @@ const { srcPath, distPath } = require('./paths')
 module.exports = {
     entry: {
         index: path.join(srcPath, 'index.js'),
+        other: path.join(srcPath, 'index.js')
     },
     module: {
         rules: [
@@ -18,9 +19,20 @@ module.exports = {
         ]
     },
     plugins: [
+        //页面一
         new HtmlWebpackPlugin({
             template: path.join(srcPath, 'index.html'),
-            filename: 'index.html'
+            filename: 'index.html',
+            // chunks 表示该页面要引用哪些 chunk （即上面的 index 和 other），默认全部引用
+            // chunks: ['index'],  // 只引用 index.js
+            excludeChunks: ['other'] //排除一些js
+        }),
+        //页面二
+        new HtmlWebpackPlugin({
+            template: path.join(srcPath, 'index.html'),
+            filename: 'other.html',
+            // chunks: ['other']  // 只引用 other.js
+            excludeChunks: ['index'] //排除一些js
         })
     ]
 }
